7a731468466d1d723388ebf18685fbff176e267a
Aquib479 <mdaquib479@gmail.com>
2024-11-15 15:32:40 +0530

    new branch

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from "./components/ui/button";
 function App() {
   return (
     <>
-      <div className="bg-red-500 p-2">
+      <div>
         <Button>Ask AI</Button>
       </div>
     </>

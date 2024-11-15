3702ae9df8d2f0098f6fba2fd14235b74bc06661
Aquib479 <mdaquib479@gmail.com>
2024-11-15 15:29:47 +0530

    Intial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="bg-red-500 p-2">
+        <Button>Ask AI</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
